e7bdd3a769e5911ec09dfb9c1adf518f9bedacbb
Anshika-Appsdeployer <anshika@appsdeployer.com>
2023-04-21 17:25:02 +0530

    Revert "Adding new components"
    This reverts commit 747ab7727d4f2aabb38974eb62e14c89e6699285.

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -7,11 +7,7 @@ const MainContainer = () => {
     <div className="items-center justify-center flex flex-col w-full h-auto">
       <HomeContainer />
 
-      <section className="w-full bg-red-500">
-        <div className="w-full flex items-center justify-between">
-          <p className="bg-white">Heullo</p>
-        </div>
-      </section>
+      {/* <section className="w-full bg-red-500"></section> */}
     </div>
   )
 }
